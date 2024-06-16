54b6635af739ae4de8d6f4b2b289a3d26ff38baa
EricNguyen1203 <21120132@student.hcmus.edu.vn>
2024-06-17 04:17:34 +0700

    add submit file icon

--- src/components/sidebar/ChatPanel.js
+++ src/components/sidebar/ChatPanel.js
@@ -17,9 +17,8 @@ const ChatMessage = ({ senderId, senderName, text, timestamp }) => {
       }}
     >
       <div
-        className={`flex ${
-          localSender ? "items-end" : "items-start"
-        } flex-col py-1 px-2 rounded-md bg-gray-700`}
+        className={`flex ${localSender ? "items-end" : "items-start"
+          } flex-col py-1 px-2 rounded-md bg-gray-700`}
       >
         <p style={{ color: "#ffffff80" }}>
           {localSender ? "You" : nameTructed(senderName, 15)}
@@ -42,6 +41,12 @@ const ChatMessage = ({ senderId, senderName, text, timestamp }) => {
 const ChatInput = ({ inputHeight }) => {
   const [message, setMessage] = useState("");
   const [file, setFile] = useState(null);
+  const inputRef = useRef(null);
+
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+
   const { publish } = usePubSub("CHAT");
   const input = useRef();
 
@@ -50,8 +55,8 @@ const ChatInput = ({ inputHeight }) => {
       className="w-full flex items-center px-2"
       style={{ height: inputHeight }}
     >
-      <div class="relative  w-full">
-        <span class="absolute inset-y-0 right-0 flex mr-2 rotate-90 ">
+      <div className="relative w-full">
+        <span className="absolute inset-y-0 right-0 flex items-center mr-2 rotate-90">
           <button
             disabled={message.length < 1}
             type="submit"
@@ -68,9 +73,7 @@ const ChatInput = ({ inputHeight }) => {
             }}
           >
             <PaperAirplaneIcon
-              className={`w-6 h-6 ${
-                message.length < 1 ? "text-gray-500 " : "text-white"
-              }`}
+              className={`w-6 h-6 ${message.length < 1 ? "text-gray-500" : "text-white"}`}
             />
           </button>
         </span>
@@ -78,7 +81,7 @@ const ChatInput = ({ inputHeight }) => {
           type="text"
           className="py-4 text-base text-white border-gray-400 border bg-gray-750 rounded pr-10 pl-2 focus:outline-none w-full"
           placeholder="Write your message"
-          autocomplete="off"
+          autoComplete="off"
           ref={input}
           value={message}
           onChange={(e) => {
@@ -88,7 +91,6 @@ const ChatInput = ({ inputHeight }) => {
             if (e.key === "Enter" && !e.shiftKey) {
               e.preventDefault();
               const messageText = message.trim();
-
               if (messageText.length > 0) {
                 publish(messageText, { persist: true });
                 setTimeout(() => {
@@ -98,6 +100,20 @@ const ChatInput = ({ inputHeight }) => {
               }
             }
           }}
+        />
+        <span className="absolute inset-y-0 right-10 flex items-center mr-2">
+          <button
+            type="button"
+            className="p-1 focus:outline-none focus:shadow-outline"
+            onClick={handleClick}
+          >
+            <PaperClipIcon className="w-6 h-6 text-gray-500" />
+          </button>
+        </span>
+        <input
+          type="file"
+          ref={inputRef}
+          style={{ display: 'none' }}
         />
       </div>
     </div>
